4c9737e2b9a4d998563d89f24bcf03bd1335af4e
jaren <jarensmart@yahoo.com>
2020-03-11 22:00:53 -0400

    init commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,19 @@
-import React, { useState } from 'react';
-
-import SavedList from './Movies/SavedList';
+import React, { useState } from "react";
+import { MovieList, Movie } from "./Movies";
+import SavedList from "./Movies/SavedList";
+import { Route } from "react-router-dom";
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+  const [savedList, setSavedList] = useState([]);
 
   const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
+    setSavedList([...savedList, movie]);
   };
 
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route exact path="/" component={MovieList} />
     </div>
   );
 };
